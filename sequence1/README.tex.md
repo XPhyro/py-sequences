91c77cdea0aaaf2788e2c3d2f236b0c7750ce64b
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2020-03-20 01:03:40 +0300

    Add sequence1

---
+++ sequence1/README.tex.md
@@ -0,0 +1,11 @@
+# Sequence
+Solutions to the Gamow-Stern Elevator Problem:
+
+$a(n) = \left\lceil{\frac{\log_10 5}{\log_10 {1 + \frac{2}{n - 2}}}}\right\rceil\ for\ n\ \geq 3.$
+
+See [A056847](https://oeis.org/A056847).
+
+# Requisites
+Written and tested with Python 3.8.1.
+
+[input_helper](https://github.com/XPhyro/input_helper) is used to get input.
---
+++ sequence1/sequence1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import time
+import datetime
+import input_helper as ih
+import math
+
+n = ih.get_int_rng("How many terms of the sequence should be computed?", low=3) 
+
+s = []
+
+# Time is obtained twice not to interfere with the actual timing of the computation.
+print(f"Initiated the computation on {datetime.datetime.now()}.\nSumming...")
+
+t0 = time.time()
+
+log5 = math.log10(5)
+for i in range(3, n):
+    s.append(math.ceil(log5 / math.log10(1 + 2 / (i - 2))))
+
+t1 = time.time()
+
+print(f"Completed the computation on {datetime.datetime.fromtimestamp(t1)}.\nWriting the results to the file \"sequence\".")
+
+s = str(s)
+
+with open("sequence", "w+") as f:
+    f.write(f"Time started\t{t0}\nTime finished\t{t1}\nTerms computed\t{n}\nSequence\t")
+    for i in s:
+        f.write(i)
+    f.write("\n")
+    f.close()
+
+print("Writing completed.")
