71a1e8c610a97bf371e5e8582ea375bef199fdf2
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2020-03-29 13:45:35 +0300

    Add sequence4 placeholder

---
+++ sequence4/README.tex.md
@@ -0,0 +1,9 @@
+# Sequence
+Partial sums of central numbers of a Moebius-binomial triangle.
+
+See [A105598](https://oeis.org/A105598).
+
+# Requisites
+Written and tested with Python 3.8.2.
+
+[input_helper](https://github.com/XPhyro/input_helper) is used to get input.
---
+++ sequence4/sequence3.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import time
+import datetime
+import math
+import input_helper as ih
+
+n = ih.get_pint("How many terms of the sequence should be computed?") 
+
+s = []
+
+# Time is obtained twice not to interfere with the actual timing of the computation.
+print(f"Initiated the computation on {datetime.datetime.now()}.\nSumming...")
+
+t0 = time.time()
+
+for i in range(2, n):
+    print("placeholder")
+
+t1 = time.time()
+
+print(f"Completed the computation on {datetime.datetime.fromtimestamp(t1)}.\nWriting the results to the file \"sequence\".")
+
+s = str(s)
+
+with open("sequence", "w+") as f:
+    f.write(f"Time started\t{t0}\nTime finished\t{t1}\nTerms computed\t{n}\nSequence\t")
+    for i in s:
+        f.write(i)
+    f.write("\n")
+    f.close()
+
+print("Writing completed.")
