1c58b7c6f1aa4bf5c97226b38eb4db9b0dbca10e
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2020-05-12 12:37:29 +0300

    Bug fix in sequence7
    Fixed a bug where the terms computed was 1 less than intended.

--- sequence7/sequence7.py
+++ sequence7/sequence7.py
@@ -5,43 +5,7 @@ import time
 import datetime
 import math
 import input_helper as ih
-
-
-def is_prime(n):
-    if n == 2:
-        return True
-    if n < 2 or not n % 2:
-        return False
-
-    for i in range(3, math.trunc(math.sqrt(n)), 2):
-        if not n % i:
-            return False
-
-    return True
-
-
-def prime_factors(n):
-    c = 0
-    while not n % 2:
-        n /= 2
-        c += 1
-
-    l = []
-    if c:
-        l.append((2, c))
-
-    for i in range(3, math.trunc(n) + 1, 2):
-        if not is_prime(i):
-            continue
-
-        c = 0
-        while not n % i:
-            n /= i
-            c += 1
-        if c:
-            l.append((i, c))
-
-    return l
+import num_helper as nh
 
 
 n = ih.get_pint("How many terms of the sequence should be computed?")
@@ -53,8 +17,8 @@ print(f"Initiated the computation on {datetime.datetime.now()}.\nSumming...")
 
 t0 = time.time()
 
-for i in range(2, n):
-    l = prime_factors(i)
+for i in range(2, n + 1):
+    l = nh.prime_factors(i)
     r = False
     for j in l:
         if j[1] > 1:
